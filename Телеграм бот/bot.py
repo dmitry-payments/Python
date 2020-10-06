011a77ebf724a40a9f06b3ebe56d3603a99a064b
dmitry-payments <72413287+dmitry-payments@users.noreply.github.com>
2020-10-07 01:45:38 +0300

    Update bot.py

--- Телеграм бот/bot.py
+++ Телеграм бот/bot.py
@@ -1,24 +1,49 @@
 import telebot
-
-TOKEN = 
+ 
+user_name = ""
+memory = ""
+state = "Обычный"
+TOKEN = ''
 bot = telebot.TeleBot(TOKEN)
-
-
+ 
+dictinary = {"Привет": "Приветствую",
+             "Пока": "Уже уходишь? {}? Я буду скучать -(".format(user_name),
+             "Как дела?": "Отлично!",
+             "Который час?": "Время действовать!"
+ 
+}
+ 
 @bot.message_handler(commands=['start'])
 def start_message(message):
+    global user_name
     print("Бот начал работу")
     bot.send_message(message.chat.id, "Бот активирован и готов к работе")
-
-
+    if not user_name:
+        print("Бот отправил запрос на получение имени юзера")
+        bot.send_message(message.chat.id, "Приветствую вас, хозяин. Как вы прикажете мне вас называть?")
+ 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
+    global memory
+    global state
+    global user_name
     print("Получено новое сообщение:", message.text)
-    if message.text == "Привет":
-        bot.send_message(message.chat.id, "Приветствую, хозяин")
-    elif message.text == "Пока":
-        bot.send_message(message.chat.id, "Уже уходишь? Я буду скучать -(")
-    else:
-        bot.send_message(message.chat.id, "Я еще слишком глуп, и не знаю что ответить...")
-
-
-bot.polling()
+    if state == "Обычный":
+        if not user_name:
+            user_name = message.text
+            print("Бот принял имя юзера")
+            bot.send_message(message.chat.id, "Приятно познакомиться, {}".format(user_name))
+        else:
+            if dictinary.get(message.text) != None:
+                bot.send_message(message.chat.id, dictinary[message.text])
+            else:
+                bot.send_message(message.chat.id, "Нипонятно чёт, хотите что бы я науччился отвечать на это?")
+                memory = message.text
+                state = "Подтверждение"
+    elif state == "Подтверждение":
+        if message.text.title() == "Да":
+            bot.send_message(message.chat.id, "Отлично, и как же мне отвечать на это?")
+            state = "Обучение"
+        else:
+            bot.send_message(message.chat.id, "Не очень то и хотелось...")
+            state = "Обычный"
